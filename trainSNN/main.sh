0553db706269010a27d613b189318c76f50e9898
MarcosOriolPago <marcos.oriol@uic.es>
2024-07-12 11:56:21 +0200

    update

--- trainSNN/main.sh
+++ trainSNN/main.sh
@@ -1,18 +1,20 @@
-# Define the epochs, model architectures, and Y_num_samples
-architectures=("256_128")
-y_num_samples=("50")
+# Parse command-line arguments
+while getopts a:y: flag
+do
+    case "${flag}" in
+        a) architectures=(${OPTARG//,/ });;
+        y) y_num_samples=(${OPTARG//,/ });;
+    esac
+done
 
 # Iterate through each combination and run the Python script
-
 for architecture in "${architectures[@]}"
 do
     for y_num_sample in "${y_num_samples[@]}"
     do
         echo "Running train_model.py with architecture=$architecture, Y_num_samples=$y_num_sample"
-        python3.9 train_model.py "50" "$architecture" "$y_num_sample" "trial_output"
+        python3.9 train_model.py "50" "$architecture" "$y_num_sample" "models"
     done
 done
 
-python3.9 plot_accuracies.py "trial_output"
-
-./update_git.sh
+python3.9 plot_accuracies.py "models"
